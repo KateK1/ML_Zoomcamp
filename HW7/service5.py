75f5f078ae4acc3d1671b660d317e9da249674ba
Ekaterina Kutovaia <80972558+KateK1@users.noreply.github.com>
2022-10-24 14:48:37 +0200

    Add files via upload

---
+++ HW7/service5.py
@@ -0,0 +1,20 @@
+import bentoml
+from bentoml.io import JSON
+from bentoml.io import NumpyNdarray
+
+model_ref = bentoml.sklearn.get("mlzoomcamp_homework:qtzdz3slg6mwwdu5")
+
+model_runner = model_ref.to_runner()
+
+svc = bentoml.Service("Q5_model", runners=[model_runner])
+
+
+@svc.api(input=NumpyNdarray(), output=JSON())
+def classify(vector):
+    prediction = model_runner.predict.run(vector)
+    print(prediction)
+    result = prediction[0]
+
+    return result
+
+    
